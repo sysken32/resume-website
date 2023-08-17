46638805ab044a9efb0d08ace5165b6178437b14
Elkan D'Silva <email.dsilva@my.centennialcollege.ca>
2023-08-17 16:05:17 -0400

    HTML document rebuild with tailwindcss

---
+++ resume-website/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+let homeBtn = document.querySelector(".home"); 
+let aboutBtn = document.querySelector(".about");
+let projectsBtn = document.querySelector(".projects");
+let contactBtn = document.querySelector(".contact");
+
+homeBtn?.addEventListener("click", () => alert("test"));

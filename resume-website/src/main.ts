9e19d73871e67a6e5d12f2b35bfa8de55f3ec5d7
Elkan D'Silva <email.dsilva@my.centennialcollege.ca>
2023-08-21 01:22:28 -0400

    progress on HTML + TypeScript. Added scroll functions/working nnav bar

--- resume-website/src/main.ts
+++ resume-website/src/main.ts
@@ -4,5 +4,31 @@ let homeBtn = document.querySelector(".home");
 let aboutBtn = document.querySelector(".about");
 let projectsBtn = document.querySelector(".projects");
 let contactBtn = document.querySelector(".contact");
+let myProjectsBtn = document.querySelector(".myProjects");
 
-homeBtn?.addEventListener("click", () => alert("test"));
+document.addEventListener("DOMContentLoaded", ()=>{ //All click events when pressed will jump to the related section
+    myProjectsBtn?.addEventListener("click", () =>{
+        let projectsSection = document.querySelector(".projects-page");
+        projectsSection?.scrollIntoView({behavior: "smooth", block: "start"});
+    });
+
+    homeBtn?.addEventListener("click", () =>{
+        let homeSection = document.querySelector(".home-page");
+        homeSection?.scrollIntoView({behavior: "smooth", block: "start"});
+    })
+    aboutBtn?.addEventListener("click", () =>{
+        let aboutSection = document.querySelector(".about-page");
+        aboutSection?.scrollIntoView({behavior: "smooth", block: "start"});
+    })
+
+    projectsBtn?.addEventListener("click", ()=>{
+        let projectsSection = document.querySelector(".projects-page");
+        projectsSection?.scrollIntoView({behavior: "smooth", block: "start"});
+    })
+
+    contactBtn?.addEventListener("click", ()=>{
+        let contactSection = document.querySelector(".contact-page");
+        contactSection?.scrollIntoView({behavior: "smooth", block: "start"});
+    })
+});
+
